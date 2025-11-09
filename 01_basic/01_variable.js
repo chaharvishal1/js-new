a4f6b685ba5e35e0f57499cac571117a1635eb47
chaharvishal1 <95270956+chaharvishal1@users.noreply.github.com>
2025-11-09 18:43:19 +0000

    push changes related to next topics

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+var name = "Vishal"
+let age = 28
+const UID = 1234
+city = "Agra"
+
+
+// {
+//     var name = "Rahul"
+// }
+
+/* Please do not use VAR because of issue occur in functional and global scope */
+
+console.log(UID)
+
+
+console.table([name, age, UID, city])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict" // Treat as all JS code as new version
+
+// alert(3 + 3) 
+// we are usin nodeJs not a browser
+
+let name = "Charli"
+let age = 23
+let isLoggedIn = false
+let state
+
+
+// number => 2^53
+// bigInt
+// string
+// boolean
+// null => standalone value
+// undefined =>
+// symbol => for uniqueness
+
+
+// object
+
+
+
+console.log(typeof null) // => o/p is object
+
+console.log(typeof undefined)  // => o/p is undefined
