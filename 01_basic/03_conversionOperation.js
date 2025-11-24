b67526fd747a238a2e39ec479cf7db89d2987990
chaharvishal1 <95270956+chaharvishal1@users.noreply.github.com>
2025-11-24 18:11:31 +0000

    push changes to conversion operator

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "33mkmk"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+
+/* ----------------------- Operation ----------------------------------- */
+
+
+
+
