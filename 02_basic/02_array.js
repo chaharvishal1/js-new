a54ecd53b23ddaef5d8c0e1e7677c28f74212037
chaharvishal1 <95270956+chaharvishal1@users.noreply.github.com>
2025-12-14 13:16:08 +0000

    commit array

---
+++ 02_basic/02_array.js
@@ -0,0 +1,19 @@
+const marvel_hero = ["Spiderman", "Thor", "Ironman"]
+
+const dc_hero = ["Superman", "Batman", "Flash"]
+
+// console.log(dc_hero, marvel_hero);
+
+
+// marvel_hero.push(dc_hero)
+
+// console.log(marvel_hero);
+// console.log(marvel_hero[3][1]);
+
+const all_new_heros =  marvel_hero.concat(dc_hero)
+
+console.log(all_new_heros);
+
+
+
+
