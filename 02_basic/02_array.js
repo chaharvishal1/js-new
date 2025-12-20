2a517c4a86cf1483d9aa22c87a5597356110844a
chaharvishal1 <95270956+chaharvishal1@users.noreply.github.com>
2025-12-20 18:09:13 +0000

    push array example

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -10,9 +10,33 @@ const dc_hero = ["Superman", "Batman", "Flash"]
 // console.log(marvel_hero);
 // console.log(marvel_hero[3][1]);
 
-const all_new_heros =  marvel_hero.concat(dc_hero)
+// const all_new_heros =  marvel_hero.concat(dc_hero)
 
-console.log(all_new_heros);
+const all_heros = [...marvel_hero, ...dc_hero]
+
+// console.log(all_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], [7, 8, 9, [10, 11]]]
+
+const real_another_array = another_array.flat(Infinity)
+
+
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("Vishal"));
+
+console.log(Array.from("Vishal Chahar"))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
+
+
+
+
 
 
 
